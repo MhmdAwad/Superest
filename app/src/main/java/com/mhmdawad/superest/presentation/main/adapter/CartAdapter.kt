f358904c37e79ebe4cacdf1ae707b7e4f342c61f
MhmdAwad <mhmdawaddd@gmail.com>
2022-04-07 02:38:43 +0200

    implement cart fragment to add products to user cart.

---
+++ app/src/main/java/com/mhmdawad/superest/presentation/main/adapter/CartAdapter.kt
@@ -0,0 +1,85 @@
+package com.mhmdawad.superest.presentation.main.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.mhmdawad.superest.BR
+import com.mhmdawad.superest.R
+import com.mhmdawad.superest.model.ProductModel
+import com.mhmdawad.superest.util.ADVANCED_SHOP_LAYOUT
+import com.mhmdawad.superest.util.SIMPLE_SHOP_LAYOUT
+import javax.inject.Inject
+
+class CartAdapter
+@Inject
+constructor() : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+
+    private val productsList = mutableListOf<ProductModel>()
+    private val purchasedProductsList = mutableListOf<ProductModel>()
+    private lateinit var productListener: ProductListener
+
+    fun addProducts(list: List<ProductModel>, listener: ProductListener) {
+        productListener = listener
+        productsList.clear()
+        purchasedProductsList.clear()
+        productsList.addAll(list)
+        purchasedProductsList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun getPurchasedProducts() = purchasedProductsList
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        return CartViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.cart_item_rv_layout, parent, false
+            )
+        )
+    }
+
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        holder.bind(productsList[position])
+    }
+
+    override fun getItemCount(): Int = productsList.size
+
+    fun onQuantityTextChanged(
+        text: CharSequence,
+        productModel: ProductModel,
+        priceTextView: TextView
+    ) {
+        val quantity = text.toString()
+        if (quantity.isNotEmpty()) {
+            val quantityNumber = quantity.toDouble()
+            if (quantityNumber > 0) {
+                val product = purchasedProductsList.single { it.id == productModel.id }
+                product.quantity = quantityNumber
+                priceTextView.text =
+                    priceTextView.context.getString(
+                        R.string.price,
+                        (productModel.price * quantityNumber)
+                    )
+            }
+        }
+    }
+
+    inner class CartViewHolder(private val binding: ViewDataBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(productModel: ProductModel) = with(binding) {
+            setVariable(BR.productItem, productModel)
+            setVariable(BR.productListener, productListener)
+            setVariable(BR.adapter, this@CartAdapter)
+        }
+    }
+
+    interface ProductListener {
+        fun onProductDelete(productModel: ProductModel)
+    }
+}
---
+++ app/src/main/java/com/mhmdawad/superest/presentation/main/fragment/cart/CartFragment.kt
@@ -0,0 +1,98 @@
+package com.mhmdawad.superest.presentation.main.fragment.cart
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.mhmdawad.superest.R
+import com.mhmdawad.superest.databinding.FragmentCartBinding
+import com.mhmdawad.superest.model.ProductModel
+import com.mhmdawad.superest.presentation.main.adapter.CartAdapter
+import com.mhmdawad.superest.util.LOADING_ANNOTATION
+import com.mhmdawad.superest.util.Resource
+import com.mhmdawad.superest.util.extention.hide
+import com.mhmdawad.superest.util.extention.show
+import com.mhmdawad.superest.util.extention.showToast
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
+
+
+@AndroidEntryPoint
+class CartFragment : Fragment(), CartAdapter.ProductListener {
+
+    private val cartViewModel by viewModels<CartViewModel>()
+    private lateinit var binding: FragmentCartBinding
+
+    @Inject
+    @Named(LOADING_ANNOTATION)
+    lateinit var loadingDialog: Dialog
+
+    @Inject
+    lateinit var cartAdapter: CartAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cartViewModel.getAllCartProducts()
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cart, container, false)
+        binding.fragment = this
+        binding.adapter = cartAdapter
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeListener()
+    }
+
+    private fun observeListener() {
+        cartViewModel.cartProductsLiveData.observe(viewLifecycleOwner, {cartProducts->
+            when(cartProducts){
+                is Resource.Success ->{
+                    if(cartProducts.data == null || cartProducts.data.isEmpty()){
+                        binding.emptyProducts.show()
+                        binding.cartContainer.hide()
+                    }else {
+                        cartAdapter.addProducts(cartProducts.data, this)
+                        binding.emptyProducts.hide()
+                        binding.cartContainer.show()
+                    }
+                    loadingDialog.hide()
+                }
+                is Resource.Loading ->{
+                    loadingDialog.show()
+                }
+                is Resource.Error->{
+                    loadingDialog.hide()
+                    showToast(cartProducts.msg!!)
+                }
+            }
+        })
+    }
+
+    // add quantity with number one on all products and add old quantity to quantity type
+    // and create track order and submit order and add payment method.
+    fun checkOutProducts(){
+        var totalPrice = 0.0
+        cartAdapter.getPurchasedProducts().forEach {
+            totalPrice += it.quantity * it.price
+        }
+        showToast(totalPrice.toString())
+    }
+
+    override fun onProductDelete(productModel: ProductModel) {
+        cartViewModel.deleteProductFromCart(productModel)
+    }
+
+
+}
